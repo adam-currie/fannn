89d8a9dfdeb792493bc177619a58e4abf87e4403
adam.currie <tfxflrstx@gmail.com>
2021-08-25 23:17:46 -0400

    updated tests

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -22,19 +22,19 @@ TEST_CASE("parseUserExp_test"){
     function<function<int(int)>(string)> getCurve = [](string s){
         REQUIRE(s == "debug_curve");
         return [](int i){ 
-            return i*10; 
+            return i*i; 
         };
     };
 
     function<int()> f = parseUserExp(
-        "input:debug_temp % 3 func:debug_curve",
+        "input:debug_temp * 2 func:debug_curve",
         getSensorDebug,
         getCurve);
         
     REQUIRE(f() == 0);
-    REQUIRE(f() == 10);
-    REQUIRE(f() == 20);
-    REQUIRE(f() == 0);
+    REQUIRE(f() == 4);
+    REQUIRE(f() == 16);
+    REQUIRE(f() == 36);
 }
 
 TEST_CASE("governor_test"){
@@ -65,10 +65,14 @@ TEST_CASE("governor_test"){
     REQUIRE(c == 2);
 }
 
-TEST_CASE("temp_test"){
-    LmSensorsReader().debugListEverything();
+TEST_CASE("sensors_get_all_test"){
+    for(string str : LmSensorsReader().getAll()){
+        cout << str << endl;
+    }
 }
 
 TEST_CASE("sysfs_pwm_writer_test"){
-    SysfsPwmWriter().getAll();
+    auto writer = SysfsPwmWriter();
+    auto devices = writer.getAll();
+    writer.setValue("/sys/devices/platform/nct6687.2592/hwmon/hwmon4/pwm1", 50);
 }
