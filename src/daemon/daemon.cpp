0abe0b0036ffbbfc1ad3eb8347b9beea6717522d
adam <tfxflrstx@gmail.com>
2021-08-30 00:52:00 -0400

    cool stepper dude encapsulates timed steps and fixed the over sleeping problem

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -3,31 +3,77 @@
 #include "sysfs_pwm_writer.h"
 #include "lm_sensors_reader.h"
 #include <sensors/error.h>
-#include <stdexcept>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+/**
+ * @brief waits just long enough to make a step take as long as it should, taking into account the time already spent on it
+ */
+class MinIntervalStepper{
+    timespec lastTime = {0,0};
+    timespec interval;
+    const long NS_IN_A_SEC = 1000000000;
+    void getTime(timespec *out){
+        if(clock_gettime(CLOCK_MONOTONIC, out))
+            throw runtime_error("clock_gettime() failed:" + errno);
+    }
+    public: 
+        MinIntervalStepper(int minItervalMs){
+            interval.tv_sec = minItervalMs / 1000;
+            interval.tv_nsec = (minItervalMs % 1000) * (NS_IN_A_SEC/1000);
+        }
+        void step(){
+            timespec current, elapsed, remaining;
+            getTime(&current);
+
+            elapsed.tv_sec = current.tv_sec - lastTime.tv_sec;
+            elapsed.tv_nsec = current.tv_nsec - lastTime.tv_nsec;
+
+            remaining.tv_nsec = interval.tv_nsec-elapsed.tv_nsec;
+            remaining.tv_sec = interval.tv_sec-elapsed.tv_sec;
+            //carry up nsec wrap arounds
+            remaining.tv_sec += remaining.tv_nsec / NS_IN_A_SEC;
+            remaining.tv_nsec %= NS_IN_A_SEC;
+
+            /*if seconds remaining was 0 but ns remaining was negative 
+              it would have rolled over and made seconds negative instead, 
+              so as long as seconds isn't negative we have waiting to do */
+            if(remaining.tv_sec >= 0){
+                nanosleep(&remaining, nullptr);
+                //add time spent sleeping
+                lastTime.tv_sec = current.tv_sec + remaining.tv_sec;
+                lastTime.tv_nsec = current.tv_nsec + remaining.tv_nsec;
+                //carry up nsec wrap arounds
+                lastTime.tv_sec += lastTime.tv_nsec / NS_IN_A_SEC;
+                lastTime.tv_nsec %= NS_IN_A_SEC;
+            }else{
+                //time spent in this function is insignificant compared to minimum usable interval
+                lastTime = current;
+            }
+        }
+};
+
 void loadConfig();
-void sleepMs(int milliSeconds);
 
 //todo: should be using an abstraction layer with prefixes on the device id str for different underlying writers
 SysfsPwmWriter writer = SysfsPwmWriter();
 //todo: should be using an abstraction layer
 LmSensorsReader reader = LmSensorsReader();
-int updateIntervalMs = 2;
+int updateIntervalMs = 2000;
 vector<function<void()>> governors;
 
 int main(){
     loadConfig();
     int i = 0;//debug
+    MinIntervalStepper stepper(updateIntervalMs);
     while(1){
-        for(auto g : governors) g();
+        stepper.step();
+        for(auto g : governors) 
+            g();
         std::cout << i++ << std::endl;//debug
-        sleepMs(updateIntervalMs);
     }
-
-    return 0;
 }
 
 void loadConfig(){
@@ -56,11 +102,4 @@ void loadConfig(){
     };
 
     governors.push_back(getGovernor(junctionTempTimes2, {pwm1Setter}));
-}
-
-void sleepMs(int milliseconds){
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
 }
