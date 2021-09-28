0a83e0379d9b9e679ce7cd377722176484c08f61
adam <tfxflrstx@gmail.com>
2021-09-28 19:47:46 -0400

    refactor

--- src/shared/headers/atomic_file_writer.h
+++ src/shared/headers/atomic_file_writer.h
@@ -6,13 +6,17 @@
 
 namespace Fannn {
 
-    class AtomicFileWriter{
+    class AtomicFileWriter {
         class Impl;
         std::experimental::propagate_const<std::unique_ptr<Impl>> pImpl;
         public:
             AtomicFileWriter(std::string filename);
             ~AtomicFileWriter();
             void atomicWrite();
+            AtomicFileWriter& operator<<(const char* s);
+            AtomicFileWriter& operator<<(const std::string& s) { 
+                return operator<<(s.c_str());
+            }
     };
 
 }
